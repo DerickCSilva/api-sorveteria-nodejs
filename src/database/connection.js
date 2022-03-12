72f6d6467b47585a60a30b1d3c30c01859e8c988
unknown <DerickCSilva>
2022-03-12 17:36:36 -0300

    feat: mudando o banco de dados

--- src/database/connection.js
+++ src/database/connection.js
@@ -5,13 +5,7 @@ const Sequelize = require('sequelize');
 const connection = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
     host: process.env.HOST,
     dialect: process.env.DIALECT,
-    timezone: process.env.TIMEZONE,
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    },
+    timezone: process.env.TIMEZONE
 });
 
 module.exports = connection;
